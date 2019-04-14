b7c8149a45b3a6460a2ab3a023dfe2e4d9c14dcf
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-14 20:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({recipes}, object, {[key]: value})
+  return Object.assign({object}, recipes, {[key]: value})
 }
